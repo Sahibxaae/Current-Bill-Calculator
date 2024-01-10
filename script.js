c314aa03f7895d368a599b85c27f12f4c753b9af
Mohamed Sahib <137259220+Sahibxaae@users.noreply.github.com>
2024-01-11 00:48:11 +0530

    script.js

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ function displayAmnt(){
         amount=unit*0.75;
     }
     else if(unit>50 && unit<=150){
-        amount+=50*0.75+(unit-50)*1.00;
+        amount=50*0.75+(unit-50)*1.00;
     }
     else if(unit>150 && unit<=250){
-        amount+=50*0.75+100*1.00+(unit-150*1.30);
+        amount=50*0.75+100*1.00+(unit-150)*1.30;
     }
     else if(unit>250){
-        amount+=50*0.75+100*1.00+100*1.30+(unit-250*1.50);
+        amount+=50*0.75+100*1.00+100*1.30+(unit-250)*1.50;
     }
     else{
          alert("Enter units");
